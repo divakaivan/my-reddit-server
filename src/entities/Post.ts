799898427cf3a1f34a9b19c90f43e0d5f56421c8
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-25 14:44:26 +0300

    add type gql field types for the Post object

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
 
+@ObjectType() // this makes the class a graphql type, otherwise wont work when we pass it in post.ts as the returned value
 @Entity()
 export class Post {
+  @Field(() => Int) // exposes the item to the gql schema
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "text" })
   title!: string;
 }
