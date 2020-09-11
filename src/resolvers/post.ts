ab358065d24eadb9551ba3fadefd78fd4fbbfe20
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-11 16:02:54 +0300

    use the dataloader userLoader. if you check, you will see that we now fetch only 2 users actually, (2 because atm there are only ben and bob as the creators), but before we had a sql query for each post's creator

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {MyContext} from "../types";
 import {isAuth} from "../middleware/isAuth";
 import {getConnection} from "typeorm";
 import {Updoot} from "../entities/Updoot";
+import {User} from "../entities/User";
 
 @InputType()
 class PostInput {
@@ -42,6 +43,16 @@ export class PostResolver {
     ) {
         return root.text.slice(0, 50)
     }
+
+    @FieldResolver(()=>User)
+    creator(
+        @Root() post: Post,
+        @Ctx() {userLoader}: MyContext
+    ) {
+        return userLoader.load(post.creatorId);
+    }
+
+
 
     @Mutation(()=>Boolean)
     @UseMiddleware(isAuth)
@@ -121,21 +132,14 @@ export class PostResolver {
         };
 
         // json_build_object lets reshape data into an object - which is what our gql expects for the creator
-        // because otherwise we get all the items on a top level
+        // because otherwise we get all the items on a top level (check previous commits to see logic)
         const posts = await getConnection().query(`
             select p.*, 
-            json_build_object(
-                    'id', u.id,
-                    'username', u.username,
-                    'email', u.email,
-                    'createdAt', u."createdAt",
-                    'updatedAt', u."updatedAt"
-                ) creator,
+
                 ${req.session.userId 
                     ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"' 
                     : 'null as "voteStatus"'}
             from post p
-            inner join public.user u on u.id = p."creatorId"
             ${cursor ? `where p."createdAt" < $${cursorIdx}` : ''}
             order by p."createdAt" DESC
             limit $1
@@ -169,7 +173,7 @@ export class PostResolver {
 
     @Query(() => Post, {nullable: true})
     post(@Arg("id", ()=>Int) id: number): Promise<Post | undefined> {
-        return Post.findOne(id, {relations: ['creator']});
+        return Post.findOne(id);
     }
 
     @Mutation(() => Post)
